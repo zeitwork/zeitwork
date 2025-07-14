2472cefd21a956a36fc6fe99c541bdf4f27f337e
Tom Härter <tomuhaerter@gmail.com>
2025-07-14 15:57:41 +0200

    Update socials

--- apps/web/app/components/marketing/navigation.vue
+++ apps/web/app/components/marketing/navigation.vue
@@ -6,8 +6,12 @@
         <!-- <MarketingButton> Sign In </MarketingButton> -->
         <!-- <MarketingButton variant="primary"> Sign Up </MarketingButton> -->
         <MarketingButton variant="secondary" to="https://github.com/zeitwork/zeitwork" target="_blank">
-          Star us on GitHub
+          GitHub
         </MarketingButton>
+        <MarketingButton variant="secondary" to="https://discord.gg/GBgRbjMDpc" target="_blank">
+          Discord
+        </MarketingButton>
+        <MarketingButton variant="secondary" to="https://x.com/zeitwork" target="_blank">X</MarketingButton>
       </div>
     </div>
   </DWrapper>
